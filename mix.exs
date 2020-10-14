875717b07761f6c474c13b909cc457f335a0446e
Marcel Otto <marcelotto@gmx.de>
2020-10-14 11:24:44 +0200

    Setup initial dev environment

--- mix.exs
+++ mix.exs
@@ -1,28 +1,81 @@
 defmodule RDF.XML.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/rdf-elixir/rdf-xml-ex"
+
+  @version File.read!("VERSION") |> String.trim()
+
   def project do
     [
       app: :rdf_xml,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: @version,
+      elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+
+      # Hex
+      package: package(),
+      description: description(),
+
+      # Docs
+      name: "RDF-XML.ex",
+      docs: [
+        main: "RDF.XML",
+        source_url: @repo_url,
+        source_ref: "v#{@version}",
+        extras: ["README.md", "CHANGELOG.md"]
+      ],
+
+      # Dialyzer
+      dialyzer: dialyzer(),
+
+      # ExCoveralls
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  defp description do
+    """
+    An implementation of RDF-XML for Elixir and RDF.ex.
+    """
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      maintainers: ["Marcel Otto"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url},
+      files: ~w[lib mix.exs README.md LICENSE.md VERSION]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def application do
+    [extra_applications: [:logger]]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rdf, "~> 0.9"},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13", only: :test}
     ]
   end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
