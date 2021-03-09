78a73a1ecc89301577bd2bb975ad1f5060f0b129
Marcel Otto <marcelotto@gmx.de>
2021-03-09 13:22:35 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -65,9 +65,9 @@ defmodule RDF.XML.MixProject do
     [
       {:rdf, "~> 0.9.1"},
       {:saxy, "~> 1.2"},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.13", only: :test},
+      {:excoveralls, "~> 0.14", only: :test},
       {:benchee, "~> 1.0", only: :dev}
     ]
   end
